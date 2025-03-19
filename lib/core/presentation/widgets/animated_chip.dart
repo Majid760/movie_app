73b1d5658fe6f7cd35f69f4fc4ad46e81a75202e
Majid760 <themuhmand@gmail.com>
2025-03-19 23:55:31 +0500

    animated(fade-in-out) chip is added

---
+++ lib/core/presentation/widgets/animated_chip.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+import '../../theme/app_colors.dart';
+
+class AnimatedChip extends StatefulWidget {
+  final Widget label;
+  final VoidCallback onRemove;
+  final bool isVisible;
+
+  const AnimatedChip({
+    super.key,
+    required this.label,
+    required this.onRemove,
+    required this.isVisible,
+  });
+
+  @override
+  State<AnimatedChip> createState() => _AnimatedChipState();
+}
+
+class _AnimatedChipState extends State<AnimatedChip> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+
+    _opacityAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut, // Smooth fade effect
+      ),
+    );
+
+    if (widget.isVisible) {
+      _controller.forward();
+    }
+  }
+
+  @override
+  void didUpdateWidget(AnimatedChip oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.isVisible != oldWidget.isVisible) {
+      if (widget.isVisible) {
+        _controller.forward();
+      } else {
+        _controller.reverse().then((_) {
+          if (mounted) {
+            widget.onRemove();
+          }
+        });
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Opacity(
+          opacity: _opacityAnimation.value,
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 32),
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+              decoration: BoxDecoration(
+                color: AppColors.grey.withValues(alpha: 0.1),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  widget.label,
+                  const SizedBox(width: 22),
+                  GestureDetector(
+                    onTap: () {
+                      _controller.reverse().then((_) {
+                        if (mounted) {
+                          widget.onRemove();
+                        }
+                      });
+                    },
+                    child: Icon(
+                      Icons.clear,
+                      size: 20,
+                      color: AppColors.textPrimary,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/features/seat_booking/presentation/pages/booking_payment_screen.dart
+++ lib/features/seat_booking/presentation/pages/booking_payment_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app_assessment/core/presentation/widgets/animated_chip.dart';
 import 'package:movie_app_assessment/core/theme/app_colors.dart';
 import 'package:movie_app_assessment/core/theme/app_typography.dart';
 import 'package:movie_app_assessment/core/utils/app_path.dart';
@@ -138,9 +139,13 @@ class _BookingPaymentScreenState extends State<BookingPaymentScreen> {
                                   : "De-selected Seat[$rowI][$colI]",
                             );
                             if (seatState == SeatState.selected) {
-                              selectedSeats.add(SeatNumber(rowI: rowI, colI: colI));
+                              setState(() {
+                                selectedSeats.add(SeatNumber(rowI: rowI, colI: colI));
+                              });
                             } else {
-                              selectedSeats.remove(SeatNumber(rowI: rowI, colI: colI));
+                              setState(() {
+                                selectedSeats.remove(SeatNumber(rowI: rowI, colI: colI));
+                              });
                             }
                           },
                           stateModel: SeatLayoutStateModel(
@@ -588,40 +593,18 @@ class _BookingPaymentScreenState extends State<BookingPaymentScreen> {
               ),
             ),
             // seat selection section
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Container(
-                margin: EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 32,
-                ),
-                padding: EdgeInsets.symmetric(
-                  horizontal: 10,
-                  vertical: 8,
-                ),
-                decoration: BoxDecoration(
-                  color: AppColors.grey.withValues(
-                    alpha: 0.1,
-                  ),
-                  borderRadius: BorderRadius.circular(
-                    10,
-                  ),
-                ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text("4 / 3 row"),
-                    const SizedBox(
-                      width: 22,
-                    ),
-                    Icon(
-                      Icons.clear,
-                      size: 20,
-                      color: AppColors.textPrimary,
-                    )
-                  ],
-                ),
-              ),
+            Wrap(
+              direction: Axis.horizontal,
+              children: selectedSeats
+                  .map((SeatNumber seat) => AnimatedChip(
+                        label: SeatRichText(
+                          row: seat.rowI,
+                          column: seat.colI,
+                        ),
+                        onRemove: () {},
+                        isVisible: true,
+                      ))
+                  .toList(),
             )
           ],
         ),
@@ -689,6 +672,38 @@ class _BookingPaymentScreenState extends State<BookingPaymentScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SeatRichText extends StatelessWidget {
+  final int row;
+  final int column;
+
+  const SeatRichText({
+    super.key,
+    required this.row,
+    required this.column,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        children: [
+          TextSpan(
+              text: column.toString(),
+              style: AppTypography.labelMedium.copyWith(fontSize: 14, color: AppColors.textPrimary)),
+          TextSpan(
+              text: ' / ',
+              style: AppTypography.labelMedium
+                  .copyWith(fontSize: 14, color: AppColors.textPrimary, fontWeight: FontWeight.w400)),
+          TextSpan(
+              text: '$row row',
+              style: AppTypography.labelMedium
+                  .copyWith(fontSize: 10, color: AppColors.textPrimary, fontWeight: FontWeight.w400)),
+        ],
       ),
     );
   }
